d912325601f8af3bfd2ac2138c980e6ee0a8207e
Александр <lugovskoia@mail.ru>
2020-12-28 00:16:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+$(function () {
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH;
+    let scrollPos = $(Window).scrollTop();
+
+    $(Window).on("scroll load resize", function(){
+        let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+            
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+        
+    });
+
+});
